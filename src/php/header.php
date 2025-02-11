5d6d31bcfe5b8eb295edad94db2f88dbf29da387
roff-dev <kieronoates@hotmail.co.uk>
2025-02-11 13:05:04 +0000

    icon cdn

--- src/php/header.php
+++ src/php/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bakers Dozen</title>
+    <script src="https://kit.fontawesome.com/80735aad70.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
