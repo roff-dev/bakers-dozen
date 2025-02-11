133918f0b0e9efe5164a384a814153181d23f0a4
roff-dev <kieronoates@hotmail.co.uk>
2025-02-11 12:31:59 +0000

    Update header.php

--- src/php/header.php
+++ src/php/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bakers  Dozen</title>
+    <title>Bakers Dozen</title>
 </head>
 <body>
 
