0e1b27c402e77327c395b41e5831547397b80943
JoshStaff99 <Joshua.Stafford@netmatters-scs.com>
2025-02-14 15:10:08 +0000

    Footer
    Added the HTML content for the footer and majority of CSS styling.

--- src/php/footer.php
+++ src/php/footer.php
@@ -1,2 +1,43 @@
+    <footer>
+        <div class="footer-container">
+
+            <div class="footer-list-container">
+
+            <div>
+                <h3><strong>Address</strong></h3>
+                <ul class="footer-list">
+                    <li>107 Glouchester Street</li>
+                    <li>Norwich, Norfolk</li>
+                    <li class="list-margin">NR2 2DY</li>
+
+                    <li>01603 123 987</li>
+                    <li>info@bakersdozen.co.uk</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3><strong>Opening Times</strong></h3>
+                <ul class="footer-list">
+                    <li>Monday - Friday 8AM - 4PM</li>
+                    <li>Saturday 8AM - 3PM</li>
+                    <li>Closed Sunday</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3><strong>Sections</strong></h3>
+                <ul class="footer-list footer-list-right">
+                    <li>About</li>
+                    <li>Products</li>
+                    <li>Testimonals</li>
+                    <li>Order</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+    
+        </div>
+            <p class="copyright">Copyright &copy; Baker's Dozen. All Rights Reserved 2019</p>
+    </footer>
 </body>
+    <script></script>
 </html>
