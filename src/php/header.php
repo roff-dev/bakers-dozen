bdf0b9ed7c336f8300fba53c8a877880a6e0fdfe
JoshR95 <joshuarickards@hotmail.co.uk>
2025-02-18 17:21:05 +0000

    finished banner
    the banner is pretty much finished, it has some alignment issues when compared to the rest of the content on the page but the rest is pretty much done

--- src/php/header.php
+++ src/php/header.php
@@ -22,16 +22,15 @@
             </div>
         </nav>
         <section class="banner">
-            <div class="banner-circle">
-                <div class="banner-content">
-                    <h2>Everyday Fresh</h2>
-                    <p>
-                        THE FINEST BREADS AND PASTRIES IN THE
-                        <BR>
-                        HEART OF NORWICH
-                    </p>
-                    <a href="#" class="main-button">ORDER NOW</a>
-                </div>
+            <div class="banner-circle"></div>
+            <div class="banner-content">
+                <h2>Everyday Fresh</h2>
+                <p>
+                    THE FINEST BREADS AND PASTRIES IN THE
+                    <BR>
+                    HEART OF NORWICH
+                </p>
+                <a href="#" class="main-button">ORDER NOW</a>
             </div>
         </section>
     </header>
