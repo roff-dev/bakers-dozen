f8a24d1557dca3433bec8ad7198f818329a59cc2
roff-dev <kieronoates@hotmail.co.uk>
2025-02-25 11:06:00 +0000

    food products layout

---
+++ src/php/food_products.php
@@ -0,0 +1,23 @@
+<!-- FOOD PRODUCTS SECTION -->
+<section>
+    <div class="our-products">
+        <h4 class="h4">our products</h4>
+        <div class="product-carousel">
+            <div>
+                <img src="https://placehold.co/250" alt="">
+                <h4 class="h4">savoury bread</h4>
+            </div>
+            <div>
+                <img src="https://placehold.co/250" alt="">
+                <h4 class="h4">pastries</h4>
+            </div>
+            <div>
+                <img src="https://placehold.co/250" alt="">
+                <h4 class="h4">cakes</h4>
+            </div>
+        </div>
+        <div class="find-more-btn">
+            <button>find out more</button>
+        </div>
+    </div>
+</section>
