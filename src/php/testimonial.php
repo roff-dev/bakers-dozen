34a30f4bb348a4786c2c9b98431d98e156a28d4f
JoshR95 <joshuarickards@hotmail.co.uk>
2025-03-27 13:10:54 +0000

    finished the base styling for all viewports on the testimonial page
    everything is responsive. might make some tweaks to things down the line, but currently everything responds and looks good in all viewports

--- src/php/testimonial.php
+++ src/php/testimonial.php
@@ -8,23 +8,45 @@ $result = $stmt->fetchAll();
 
 <main class="testimonial-page">
     <h1>What our customers say</h1>
-
-    <div class="testimonials">
+    <div class="testimonial-page-content">
+        
         <?php
-            if(count($result) > 0){
-                foreach($result as $row){
-                    echo '<div class="review">';
-                    echo '<p>"' . htmlspecialchars($row["content"]) . '"</p>';
-                    echo '<h4>- ' . htmlspecialchars($row["name"]) . '</h4>';
-                    echo '</div>';
-                }
-            } else {
-                echo "<p>No testimonials yet</p>";
-            }
+        // success message on succesful form submit
+        if (isset($_GET['success']) && $_GET['success'] == 1){
+            echo '<div class="success-message>"
+                    Thank you for giving your feedback!
+                </div>';
+        }
         ?>
-    </div>
 
-    <div>
+        <div class="testimonials">
+            <?php
+                if(count($result) > 0){
+                    foreach($result as $row){
+                        echo '<div class="review">';
+                        echo '<p>"' . htmlspecialchars($row["content"]) . '"</p>';
+                        echo '<h4>- ' . htmlspecialchars($row["name"]) . '</h4>';
+                        echo '</div>';
+                    }
+                } else {
+                    echo "<p>No testimonials yet</p>";
+                }
+            ?>
+        </div>
 
+        <div class="add-testimonial">
+            <h2>Share your experience</h2>
+            <form class="testimonial-form" action="submit_testimonial.php" method="POST">
+                <div class="testimonial-form-div">
+                    <!-- <label for="name">Your Name:</label> -->
+                    <input type="text" id="name" name="name" placeholder="Your Name" required>
+                </div>
+                <div class="testimonial-form-div">
+                    <!-- <label for="testimonial">Your Review:</label> -->
+                    <textarea id="testimonial" name="testimonial" rows="5" placeholder="Your Review" required></textarea>
+                </div>
+                <button class="testimonial-form-button" type="submit">Submit Review</button>
+            </form>
+        </div>
     </div>
 </main>
