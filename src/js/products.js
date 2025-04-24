18405f8bc632088b56472a2e90747f27352f0da5
roff-dev <kieronoates@hotmail.co.uk>
2025-04-24 17:03:25 +0100

    styling changes

--- src/js/products.js
+++ src/js/products.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
     $('.product-carousel').slick({
         slidesToShow: 3,  // Number of slides visible at once
-        slidesToScroll:3, // Number of slides to scroll at a time
+        slidesToScroll: 3, // Number of slides to scroll at a time
         // autoplay: true,    // Auto-play enabled
         // autoplaySpeed: 2000, // Auto-play speed in milliseconds
         dots: false,        // Show navigation dots
         arrows: true,      // Show navigation arrows
         // useCSS: false,
-        
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
